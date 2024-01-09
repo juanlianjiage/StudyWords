88a2af639e35ea0c07987c4b6e3ae22af9981cc6
眷恋嘉哥 <2521856799@qq.com>
2024-01-09 20:16:34 +0800

    Merge remote-tracking branch 'english/master'

---
+++ src/main/java/com/example/english_test/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.example.english_test.config;
+
+import com.example.english_test.entity.Cet4Word;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Cet4Word> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, Cet4Word> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/com/example/english_test/config/RedissonConfig.java
@@ -0,0 +1,17 @@
+package com.example.english_test.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://192.168.136.130:6379").setPassword("123456");
+        return Redisson.create(config);
+    }
+}
